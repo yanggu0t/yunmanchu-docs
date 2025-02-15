99f1221ed5cadebcca9e582ce1c88c3b87cbf95b
yanggu0t <zxc225751@gmail.com>
2025-02-16 02:46:51 +0800

    feat: customize navigation and search for chinese

--- app/api/search/route.ts
+++ app/api/search/route.ts
@@ -1,4 +1,12 @@
-import { createFromSource } from 'fumadocs-core/search/server';
-import { source } from '@/lib/source';
+import { NextRequest, NextResponse } from 'next/server';
+import { filterSearchResults, normalize } from '@/lib/utils';
 
-export const { GET } = createFromSource(source);
+// export const { GET } = createFromSource(source);
+
+export async function GET(req: NextRequest) {
+  const query = normalize(req.nextUrl.searchParams.get('query') ?? '');
+
+  const results = filterSearchResults(query);
+
+  return NextResponse.json(results);
+}
--- app/layout.config.tsx
+++ app/layout.config.tsx
@@ -1,4 +1,13 @@
 import type { BaseLayoutProps } from 'fumadocs-ui/layouts/shared';
+import {
+  Calendar,
+  CheckCircle,
+  Coffee,
+  Flower,
+  Home,
+  Info,
+  MapPinned,
+} from 'lucide-react';
 
 /**
  * Shared layout configurations
@@ -9,25 +18,68 @@ import type { BaseLayoutProps } from 'fumadocs-ui/layouts/shared';
  */
 export const baseOptions: BaseLayoutProps = {
   nav: {
-    title: (
-      <>
-        <svg
-          width="24"
-          height="24"
-          xmlns="http://www.w3.org/2000/svg"
-          aria-label="Logo"
-        >
-          <circle cx={12} cy={12} r={12} fill="currentColor" />
-        </svg>
-        My App
-      </>
-    ),
+    title: '蘊慢築民宿',
   },
   links: [
     {
-      text: 'Documentation',
-      url: '/docs',
-      active: 'nested-url',
+      type: 'menu',
+      text: '基本介紹',
+      items: [
+        {
+          icon: <Info />,
+          text: '關於我們',
+          description: '認識蘊慢築的故事與理念',
+          url: '/docs/introductions/about',
+        },
+        {
+          icon: <Home />,
+          text: '房間介紹',
+          description: '了解我們特色房型的獨特魅力',
+          url: '/docs/introductions/rooms',
+        },
+      ],
+    },
+    {
+      type: 'menu',
+      text: '住宿指南',
+      items: [
+        {
+          icon: <CheckCircle />,
+          text: '入住須知',
+          description: '入住相關規定與注意事項',
+          url: '/docs/guides/check_in',
+        },
+        {
+          icon: <Calendar />,
+          text: '訂房須知',
+          description: '預訂流程與訂房政策說明',
+          url: '/docs/guides/booking',
+        },
+        {
+          icon: <MapPinned />,
+          text: '交通方式',
+          description: '如何抵達蘊慢築民宿',
+          url: '/docs/guides/transport',
+        },
+      ],
+    },
+    {
+      type: 'menu',
+      text: '體驗課程',
+      items: [
+        {
+          icon: <Flower />,
+          text: '插花體驗',
+          description: '體驗花藝之美，創造專屬作品',
+          url: '/docs/experiences/floral',
+        },
+        {
+          icon: <Coffee />,
+          text: '品茶體驗',
+          description: '品味東方茶道文化的精髓',
+          url: '/docs/experiences/tea',
+        },
+      ],
     },
   ],
 };
